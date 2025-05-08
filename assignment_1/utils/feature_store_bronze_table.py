bf367be3e3c544124cabd7c263a4fd8c0a60d9fb
GUAN LIANG TAY XIYNQ <G112U5991@sdkgr.comnet>
2025-05-08 22:32:48 +0800

    add assignment_1 folder

---
+++ assignment_1/utils/feature_store_bronze_table.py
@@ -0,0 +1,52 @@
+import os
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import pprint
+import pyspark
+import pyspark.sql.functions as F
+import argparse
+
+from pyspark.sql.functions import col
+from pyspark.sql.types import StringType, IntegerType, FloatType, DateType
+
+
+def feature_store_bronze_table(snapshot_date_str, bronze_feature_store_directory, spark):
+    # prepare arguments
+    snapshot_date = datetime.strptime(snapshot_date_str, "%Y-%m-%d")
+    
+    # connect to source back end - IRL connect to back end source system
+    csv_file_path = "data/feature_clickstream.csv"
+
+    # load data - IRL ingest from back end source system
+    clickstream_df = spark.read.csv(csv_file_path, header=True, inferSchema=True).filter(col('snapshot_date') == snapshot_date)
+    
+
+    # connect to source back end - IRL connect to back end source system
+    csv_file_path = "data/features_attributes.csv"
+
+    # load data - IRL ingest from back end source system
+    attribute_df = spark.read.csv(csv_file_path, header=True, inferSchema=True).filter(col('snapshot_date') == snapshot_date)
+
+    # connect to source back end - IRL connect to back end source system
+    csv_file_path = "data/features_financials.csv"
+
+    # load data - IRL ingest from back end source system
+    financial_df = spark.read.csv(csv_file_path, header=True, inferSchema=True).filter(col('snapshot_date') == snapshot_date)
+
+    # Left Join on Customer_ID and snapshot_date
+    features_df = clickstream_df.join(attribute_df, on=['Customer_ID', 'snapshot_date'], how='outer')
+    df = features_df.join(financial_df, on=['Customer_ID', 'snapshot_date'], how='outer')
+    print(snapshot_date_str + 'row count:', df.count())
+
+    # save bronze table to datamart - IRL connect to database to write
+    partition_name = "bronze_feature_store_daily_" + snapshot_date_str.replace('-','_') + '.csv'
+    filepath = bronze_feature_store_directory + partition_name
+    df.toPandas().to_csv(filepath, index=False)
+    print('saved to:', filepath)
+
+    return df
