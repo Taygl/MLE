08dfc2c140609b115b720a904a1125a144ff4641
GUAN LIANG TAY XIYNQ <G112U5991@sdkgr.comnet>
2025-05-08 22:25:42 +0800

    edit assignment 1

--- assignment_1/main.py
+++ assignment_1/main.py
@@ -2,6 +2,7 @@ import os
 import glob
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import random
 from datetime import datetime, timedelta
@@ -13,9 +14,12 @@ import pyspark.sql.functions as F
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import StringIndexer, OneHotEncoder
 
-from pyspark.sql.functions import col, row_number, to_date, count, percentile_approx, desc, udf
-from pyspark.sql.types import StringType, IntegerType, FloatType, DateType, DoubleType, ArrayType
+from pyspark.ml.linalg import SparseVector
+from pyspark.sql.functions import sum as _sum
+from pyspark.sql.functions import col, row_number, to_date, count, percentile_approx, desc, udf, collect_set, array_contains, lit
+from pyspark.sql.types import StringType, IntegerType, FloatType, DateType, DoubleType, ArrayType, NumericType
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
+from pyspark.sql.types import *
 from pyspark.sql.window import Window
 from pyspark.sql.functions import regexp_extract, regexp_replace, when, trim, initcap, round, lower, split, explode, array_distinct, array_sort, concat_ws, expr
 
@@ -23,17 +27,21 @@ import utils.data_processing_bronze_table
 import utils.data_processing_silver_table
 import utils.data_processing_gold_table
 
-import utils.clickstream_bronze_table
-import utils.clickstream_silver_table
-import utils.clickstream_gold_table
+import utils.feature_store_bronze_table
+import utils.feature_store_silver_table
+import utils.feature_store_gold_table
 
-import utils.features_attributes_bronze_table
-import utils.features_attributes_silver_table
-import utils.features_attributes_gold_table
+# import utils.clickstream_bronze_table
+# import utils.clickstream_silver_table
+# import utils.clickstream_gold_table
 
-import utils.features_financials_bronze_table
-import utils.features_financials_silver_table
-import utils.features_financials_gold_table
+# import utils.features_attributes_bronze_table
+# import utils.features_attributes_silver_table
+# import utils.features_attributes_gold_table
+
+# import utils.features_financials_bronze_table
+# import utils.features_financials_silver_table
+# import utils.features_financials_gold_table
 
 
 # Initialize SparkSession
@@ -119,119 +127,156 @@ df = spark.read.option("header", "true").parquet(*files_list)
 print("gold label store row_count:",df.count())
 
 
-# Clickstream
-print('Clickstream')
 # create bronze datalake
-bronze_clickstream_directory = "datamart/bronze/clickstream/"
+bronze_feature_store_directory = "datamart/bronze/feature_store/"
 
-if not os.path.exists(bronze_clickstream_directory):
-    os.makedirs(bronze_clickstream_directory)
+if not os.path.exists(bronze_feature_store_directory):
+    os.makedirs(bronze_feature_store_directory)
 
 # run bronze backfill
 for date_str in dates_str_lst:
-    utils.clickstream_bronze_table.feature_clickstream_bronze_table(date_str, bronze_clickstream_directory, spark)
+    utils.feature_store_bronze_table.feature_store_bronze_table(date_str, bronze_feature_store_directory, spark)
 
 # create silver datalake
-silver_clickstream_directory = "datamart/silver/clickstream/"
+silver_feature_store_directory = "datamart/silver/feature_store/"
 
-if not os.path.exists(silver_clickstream_directory):
-    os.makedirs(silver_clickstream_directory)
+if not os.path.exists(silver_feature_store_directory):
+    os.makedirs(silver_feature_store_directory)
 
 # run silver backfill
 for date_str in dates_str_lst:
-    utils.clickstream_silver_table.feature_clickstream_silver_table(date_str, bronze_clickstream_directory, silver_clickstream_directory, spark)
-
-
-# create silver datalake
-gold_clickstream_directory = "datamart/gold/clickstream/"
-
-if not os.path.exists(gold_clickstream_directory):
-    os.makedirs(gold_clickstream_directory)
-
-# run gold backfill
-for date_str in dates_str_lst:
-    utils.clickstream_gold_table.feature_clickstream_gold_table(date_str, silver_clickstream_directory, gold_clickstream_directory, spark)
-
-
-folder_path = gold_clickstream_directory
-files_list = [folder_path+os.path.basename(f) for f in glob.glob(os.path.join(folder_path, '*'))]
-df = spark.read.option("header", "true").parquet(*files_list)
-print("clickstream row_count:",df.count())
-
-# features attributes
-print('features attributes')
-# create bronze datalake
-bronze_features_attributes_directory = "datamart/bronze/features_attributes/"
-
-if not os.path.exists(bronze_features_attributes_directory):
-    os.makedirs(bronze_features_attributes_directory)
-
-# run bronze backfill
-for date_str in dates_str_lst:
-    utils.features_attributes_bronze_table.features_attributes_bronze_table(date_str, bronze_features_attributes_directory, spark)
-
-# create silver datalake
-silver_features_attributes_directory = "datamart/silver/features_attributes/"
-
-if not os.path.exists(silver_features_attributes_directory):
-    os.makedirs(silver_features_attributes_directory)
-
-# run silver backfill
-for date_str in dates_str_lst:
-    utils.features_attributes_silver_table.features_attributes_silver_table(date_str, bronze_features_attributes_directory, silver_features_attributes_directory, spark)
+    utils.feature_store_silver_table.feature_store_silver_table(date_str, bronze_feature_store_directory, silver_feature_store_directory, spark)
 
 # create gold datalake
-gold_features_attributes_directory = "datamart/gold/features_attributes/"
+gold_feature_store_directory = "datamart/gold/feature_store/"
 
-if not os.path.exists(gold_features_attributes_directory):
-    os.makedirs(gold_features_attributes_directory)
-
-# run gold backfill
-for date_str in dates_str_lst:
-    utils.features_attributes_gold_table.features_attributes_gold_table(date_str, silver_features_attributes_directory, gold_features_attributes_directory, spark)
-
-
-folder_path = gold_features_attributes_directory
-files_list = [folder_path+os.path.basename(f) for f in glob.glob(os.path.join(folder_path, '*'))]
-df = spark.read.option("header", "true").parquet(*files_list)
-print("features_attributes row_count:",df.count())
-
-
-# features financials
-print('features financials')
-# create bronze datalake
-bronze_features_financials_directory = "datamart/bronze/features_financials/"
-
-if not os.path.exists(bronze_features_financials_directory):
-    os.makedirs(bronze_features_financials_directory)
-
-# run bronze backfill
-for date_str in dates_str_lst:
-    utils.features_financials_bronze_table.features_financials_bronze_table(date_str, bronze_features_financials_directory, spark)
-
-# create silver datalake
-silver_features_financials_directory = "datamart/silver/features_financials/"
-
-if not os.path.exists(silver_features_financials_directory):
-    os.makedirs(silver_features_financials_directory)
-
-# run silver backfill
-for date_str in dates_str_lst:
-    utils.features_financials_silver_table.features_financials_silver_table(date_str, bronze_features_financials_directory, silver_features_financials_directory, spark)
-
-
-# create gold datalake
-gold_features_financials_directory = "datamart/gold/features_financials/"
-
-if not os.path.exists(gold_features_financials_directory):
-    os.makedirs(gold_features_financials_directory)
+if not os.path.exists(gold_feature_store_directory):
+    os.makedirs(gold_feature_store_directory)
 
 # run gold backfill
 for date_str in dates_str_lst:
-    utils.features_financials_gold_table.features_financials_gold_table(date_str, silver_features_financials_directory, gold_features_financials_directory, spark)
+    utils.feature_store_gold_table.feature_store_gold_table(date_str, silver_feature_store_directory, gold_feature_store_directory, spark)
 
 
-folder_path = gold_features_financials_directory
+folder_path = gold_feature_store_directory
 files_list = [folder_path+os.path.basename(f) for f in glob.glob(os.path.join(folder_path, '*'))]
 df = spark.read.option("header", "true").parquet(*files_list)
-print("features_financials row_count:",df.count())
+print("row_count:",df.count())
+
+
+# # Clickstream
+# print('Clickstream')
+# # create bronze datalake
+# bronze_clickstream_directory = "datamart/bronze/clickstream/"
+
+# if not os.path.exists(bronze_clickstream_directory):
+#     os.makedirs(bronze_clickstream_directory)
+
+# # run bronze backfill
+# for date_str in dates_str_lst:
+#     utils.clickstream_bronze_table.feature_clickstream_bronze_table(date_str, bronze_clickstream_directory, spark)
+
+# # create silver datalake
+# silver_clickstream_directory = "datamart/silver/clickstream/"
+
+# if not os.path.exists(silver_clickstream_directory):
+#     os.makedirs(silver_clickstream_directory)
+
+# # run silver backfill
+# for date_str in dates_str_lst:
+#     utils.clickstream_silver_table.feature_clickstream_silver_table(date_str, bronze_clickstream_directory, silver_clickstream_directory, spark)
+
+
+# # create silver datalake
+# gold_clickstream_directory = "datamart/gold/clickstream/"
+
+# if not os.path.exists(gold_clickstream_directory):
+#     os.makedirs(gold_clickstream_directory)
+
+# # run gold backfill
+# for date_str in dates_str_lst:
+#     utils.clickstream_gold_table.feature_clickstream_gold_table(date_str, silver_clickstream_directory, gold_clickstream_directory, spark)
+
+
+# folder_path = gold_clickstream_directory
+# files_list = [folder_path+os.path.basename(f) for f in glob.glob(os.path.join(folder_path, '*'))]
+# df = spark.read.option("header", "true").parquet(*files_list)
+# print("clickstream row_count:",df.count())
+
+# # features attributes
+# print('features attributes')
+# # create bronze datalake
+# bronze_features_attributes_directory = "datamart/bronze/features_attributes/"
+
+# if not os.path.exists(bronze_features_attributes_directory):
+#     os.makedirs(bronze_features_attributes_directory)
+
+# # run bronze backfill
+# for date_str in dates_str_lst:
+#     utils.features_attributes_bronze_table.features_attributes_bronze_table(date_str, bronze_features_attributes_directory, spark)
+
+# # create silver datalake
+# silver_features_attributes_directory = "datamart/silver/features_attributes/"
+
+# if not os.path.exists(silver_features_attributes_directory):
+#     os.makedirs(silver_features_attributes_directory)
+
+# # run silver backfill
+# for date_str in dates_str_lst:
+#     utils.features_attributes_silver_table.features_attributes_silver_table(date_str, bronze_features_attributes_directory, silver_features_attributes_directory, spark)
+
+# # create gold datalake
+# gold_features_attributes_directory = "datamart/gold/features_attributes/"
+
+# if not os.path.exists(gold_features_attributes_directory):
+#     os.makedirs(gold_features_attributes_directory)
+
+# # run gold backfill
+# for date_str in dates_str_lst:
+#     utils.features_attributes_gold_table.features_attributes_gold_table(date_str, silver_features_attributes_directory, gold_features_attributes_directory, spark)
+
+
+# folder_path = gold_features_attributes_directory
+# files_list = [folder_path+os.path.basename(f) for f in glob.glob(os.path.join(folder_path, '*'))]
+# df = spark.read.option("header", "true").parquet(*files_list)
+# print("features_attributes row_count:",df.count())
+
+
+# # features financials
+# print('features financials')
+# # create bronze datalake
+# bronze_features_financials_directory = "datamart/bronze/features_financials/"
+
+# if not os.path.exists(bronze_features_financials_directory):
+#     os.makedirs(bronze_features_financials_directory)
+
+# # run bronze backfill
+# for date_str in dates_str_lst:
+#     utils.features_financials_bronze_table.features_financials_bronze_table(date_str, bronze_features_financials_directory, spark)
+
+# # create silver datalake
+# silver_features_financials_directory = "datamart/silver/features_financials/"
+
+# if not os.path.exists(silver_features_financials_directory):
+#     os.makedirs(silver_features_financials_directory)
+
+# # run silver backfill
+# for date_str in dates_str_lst:
+#     utils.features_financials_silver_table.features_financials_silver_table(date_str, bronze_features_financials_directory, silver_features_financials_directory, spark)
+
+
+# # create gold datalake
+# gold_features_financials_directory = "datamart/gold/features_financials/"
+
+# if not os.path.exists(gold_features_financials_directory):
+#     os.makedirs(gold_features_financials_directory)
+
+# # run gold backfill
+# for date_str in dates_str_lst:
+#     utils.features_financials_gold_table.features_financials_gold_table(date_str, silver_features_financials_directory, gold_features_financials_directory, spark)
+
+
+# folder_path = gold_features_financials_directory
+# files_list = [folder_path+os.path.basename(f) for f in glob.glob(os.path.join(folder_path, '*'))]
+# df = spark.read.option("header", "true").parquet(*files_list)
+# print("features_financials row_count:",df.count())
